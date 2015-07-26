a8a437d0a6d36342f611e9b56c970a4f5e25a8d0
andrew-stebbing <andrewstebbing@yahoo.co.uk>
2015-07-26 11:22:00 +0100

    The R programming script for the course project.

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,17 @@
 # A script to create a tidy, summary set of data from the 
 # Human Activity Recognition Using Smartphones Dataset Version 1.0 dataset.
 
+# ****************************************************************************************** 
+
+# Note: this script can only be run as long as the Samsung data is in your working directory
+
+# ******************************************************************************************
+
 # Step 1
 library(dplyr)
 library(tidyr)
 
 # Step 2
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileUrl, "dataset.zip", method = "curl")
-unzip("dataset.zip")
-
-# Step 3
 data_folder <- "./UCI HAR Dataset/"
 features <- read.table(paste0(data_folder, "features.txt"), stringsAsFactors = FALSE)
 activities <- read.table(paste0(data_folder, "activity_labels.txt"))
@@ -22,7 +23,7 @@ test_y <- read.table(paste0(data_folder, folder, "/y_", folder, ".txt" ))
 test_s <- read.table(paste0(data_folder, folder, "/subject_", folder, ".txt" ))
 
 # Assemble test files
-names(test_x) <- unlist(features[2]) # Step 4
+names(test_x) <- unlist(features[2]) # Step 3
 names(test_y) <- "activity"
 names(test_s) <- "subject"
 
@@ -36,46 +37,37 @@ train_y <- read.table(paste0(data_folder, folder, "/y_", folder, ".txt" ))
 train_s <- read.table(paste0(data_folder, folder, "/subject_", folder, ".txt" ))
 
 # Assemble train files
-names(train_x) <- unlist(features[2]) # Step 4
+names(train_x) <- unlist(features[2]) # Step 3
 names(train_y) <- "activity"
 names(train_s) <- "subject"
 
 train_all <- cbind(train_s, train_y, train_x)
 rm(train_x, train_y, train_s)
 
-# Step 5
+# Step 4
 data_all <- rbind(train_all, test_all)
 rm(train_all, test_all)
 
-# Step 6
+# Step 5
 data_all <- tbl_df(data_all)
 
-if(all(colSums(is.na(data_all))==0)) message("\nThe data frame has no missing values\n")
-# returns [1] TRUE
-
-# STEP 7
+# STEP 6
 n <- grep("mean[^F]|std|subject|activity", names(data_all))
 data_all <- data_all[, n]
 
-# STEP 8
+# STEP 7
 act <- activities[match(data_all$activity, activities$V1), 2]
 data_all$activity <- act
 
-# STEP 9
-molten_data <- gather(data_all, sample, reading, -subject, -activity)
+# STEPS 8 - 12
+data_all %>%
+    gather(sample, reading, -subject, -activity) %>%
+    mutate(statistic = factor( regexpr("mean", .$sample) < 0, labels = c("average_mean", "average_standard_deviation"))) %>%
+    group_by(subject, activity, statistic) %>%
+    summarise(average_for_activity = mean(reading, na.rm = TRUE)) %>%
+    spread(statistic, average_for_activity) %>%
+    write.table(file = "summary.txt", row.names = FALSE)
 
-# Step 10
-molten_data <- mutate(molten_data,
-                       statistic = factor( regexpr("mean", molten_data$sample) < 0,
-                                           labels = c("average_mean", "average_standard_deviation")))
-# Step 11
-summary <- summarise(group_by(molten_data, subject, activity, statistic), average_for_activity = mean(reading, na.rm = TRUE))
-
-# Step 12
-summary <- spread(summary, statistic, average_for_activity)
-
-# Step 13
-save(summary, file = "summary.RData")
-
+# **** END OF SCRIPT ****
 
 
